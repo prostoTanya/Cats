d722138b2ead5326bc0f8ab22b46bb7186381bb1
prostoTanya <tyakovleva2014@gmail.com>
2025-07-28 15:02:48 +0300

    Создано окно и виджеты

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,22 @@
 from tkinter import *
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
+
+from pygame.examples.aliens import load_image
+
+window = Tk()
+window.title('Котики')
+window.geometry('600x480')
+
+lab = Label()
+lab.pack()
+
+url ='https://cataas.com/cat'
+img = load_image(url)
+
+if img:
+    lab.config(image=img)
+    lab.image = img
+
+window.mainloop()
